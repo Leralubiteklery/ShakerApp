e8acf254240a028342cba9f17a3af39f766b463d
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2025-05-13 17:38:49 +0300

    Created custom table view cell.

---
+++ ShakerApp/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  ShakerApp
+//
+//  Created by Lera Savchenko on 13.05.25.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cocktailImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ingridientsLabel: UILabel!
+    
+    
+}
