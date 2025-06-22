4f84265aba1986d785ae3c228e8dc09dfe1e7a8a
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2025-06-22 16:12:51 +0300

    Created StorageManager.

---
+++ ShakerApp/Managers/StorageManager.swift
@@ -0,0 +1,34 @@
+//
+//  StorageManager.swift
+//  ShakerApp
+//
+//  Created by Lera Savchenko on 22.06.25.
+//
+
+import Foundation
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    private let defaults = UserDefaults.standard
+    private let favouriteKey = "favouriteKey"
+    
+    func save(drinkId: String) {
+        var favouriteIds = fetchFavouriteIds()
+        favouriteIds.append(drinkId)
+        defaults.set(favouriteIds, forKey: favouriteKey)
+    }
+    
+    func fetchFavouriteIds() -> [String] {
+        if let favouriteIds = defaults.value(forKey: favouriteKey) as? [String] {
+            return favouriteIds
+        }
+        return []
+    }
+    
+    func deleteId(at index: Int) {
+        var favouriteIds = fetchFavouriteIds()
+        favouriteIds.remove(at: index)
+        defaults.set(favouriteIds, forKey: favouriteKey)
+    }
+}
